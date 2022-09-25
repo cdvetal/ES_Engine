b62feeb631570ef0eb91f498d264851447bd71b4
lmagoncalo <lmagoncalo@hotmail.com>
2022-09-26 00:16:29 +0100

    - Update on BigGAN test

--- test_biggan.py
+++ test_biggan.py
@@ -3,6 +3,7 @@ import torch
 from torch import optim
 from torchvision import transforms
 import torchvision.transforms.functional as TF
+import torch.nn.functional as F
 
 from render.biggan import BigGAN
 from utils import CondVectorParameters
@@ -24,16 +25,18 @@ num_cuts = 128
 normalize = transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
 
 a = torch.rand(num_latents, 256)
-conditional_vector = CondVectorParameters(a, num_latents=num_latents).to(device)
+a.requires_grad = True
+# conditional_vector = CondVectorParameters(a, num_latents=num_latents).to(device)
 
-optimizer = optim.Adam(conditional_vector.parameters(), lr=0.07)
+# optimizer = optim.Adam(conditional_vector.parameters(), lr=0.07)
+optimizer = optim.Adam([a], lr=0.07)
 
 for i in range(100):
     print(i)
 
-    optimizer.zero_grad()
-    cond_vector = conditional_vector()
-    out = model(cond_vector, 1)
+    # cond_vector = conditional_vector()
+    # out = model(cond_vector, 1)
+    out = model(a, 1)
 
     p_s = []
     _, channels, sideX, sideY = out.shape
@@ -49,13 +52,15 @@ for i in range(100):
     into = normalize((into + 1) / 2)
     iii = perceptor.encode_image(into)  # 128 x 512
 
-    cos_similarity = torch.cosine_similarity(text_features, iii, dim=-1).mean()
+    cos_similarity = F.cosine_similarity(text_features, iii, dim=-1).mean()
     cos_similarity = -100 * cos_similarity
 
+    optimizer.zero_grad()
     cos_similarity.backward()
     optimizer.step()
 
-cond_vector = conditional_vector()
-out = model(cond_vector, 1)
+# cond_vector = conditional_vector()
+# out = model(cond_vector, 1)
+out = model(a, 1)
 out = TF.to_pil_image(out)
 out.save("out.png")
