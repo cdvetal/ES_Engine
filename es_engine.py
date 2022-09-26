3743c6f52fb7d5cab64f42bd60385c2166ccebb3
lmagoncalo <lmagoncalo@hotmail.com>
2022-09-26 13:13:41 +0100

    - Out of memory error 2

--- es_engine.py
+++ es_engine.py
@@ -161,8 +161,9 @@ def fitness_clip_prompts(args, img):
     """
     normalize = torchvision.transforms.Normalize((0.48145466, 0.4578275, 0.40821073),
                                                  (0.26862954, 0.26130258, 0.27577711))
+    resize = torchvision.transforms.Resize((224, 224))
     # into = normalize((into + 1) / 2)
-    into = normalize((t_img + 1) / 2)
+    into = resize(normalize((t_img + 1) / 2))
 
     image_features = args.clip.encode_image(into)
     text_clip_loss = torch.cosine_similarity(args.text_features, image_features, dim=-1).mean()
