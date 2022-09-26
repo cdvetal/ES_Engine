75e8d81a9107835979c3db31bd1583968a09e1fd
lmagoncalo <lmagoncalo@hotmail.com>
2022-09-26 12:45:27 +0100

    - Updated BigGAN test 3

--- test_biggan.py
+++ test_biggan.py
@@ -23,7 +23,7 @@ with torch.no_grad():
     text_features = perceptor.encode_text(text_inputs)
 
 num_latents = len(model.config.layers) + 1
-num_cuts = 128
+num_cuts = 32
 normalize = transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
 
 a = torch.rand(num_latents, 256)
